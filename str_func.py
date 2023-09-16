5bdecfc69f3926dbd3f0ead81a908a840b49aae1
NickKrlv <Nick.krlv@yandex.ru>
2023-09-16 08:43:33 +0300

    add upper_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def title_func(some_text):
+    """Добавил докстриг, который будет конфликтовать"""
     return some_text.title()
+
+
+def upper_func(some_text):
+    """Принимает строку и возвращает ее в верхнем регистре"""
+    return some_text.upper()
