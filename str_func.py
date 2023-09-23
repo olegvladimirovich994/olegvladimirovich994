3ba1b615fb81e8970f87a1fe5dfc1a392e47d48c
Oleg <145197203+olegvladimirovich994@users.noreply.github.com>
2023-09-23 20:05:20 +0400

    1

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def upper_func(some_text):
+    """Принимает строку и возвращает ее в верхнем регистре"""
+    return some_text.upper()
+
+
+def title_func(some_text):
+    """Принимает строку и возвращает с заглавными буквами"""
+    return some_text.title()
